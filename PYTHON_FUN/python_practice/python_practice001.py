cfc936e9b58f41626b7c7f2620d98237db8619f5
Mike Magruder <mike_magruder@yahoo.com>
2021-12-29 15:46:25 -0800

    Python Practice

---
+++ PYTHON_FUN/python_practice/python_practice001.py
@@ -0,0 +1,21 @@
+num1 = 2.2949492
+num2 = 3.7
+
+sum = num1 + num2
+
+print(type(num1))
+print(type(num2))
+print(type(sum))
+print('%.7f' %sum)
+
+#######
+
+foobar = 3.141592
+print(f'My number is {foobar:.2f} - look at the nice rounding!')
+
+## My number is 3.14 - look at the nice rounding!
+
+#######
+
+a = 6.789809823
+print('%.4f' %a)
