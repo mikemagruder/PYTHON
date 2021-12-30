376de973d9499fb58005013adf46338087e4ab6f
Mike Magruder <mike_magruder@yahoo.com>
2021-12-29 20:44:38 -0800

    Python String Practice

--- PYTHON_FUN/python_practice/hello_world.py
+++ PYTHON_FUN/python_practice/hello_world.py
@@ -23,3 +23,37 @@ fave_food1 = "sushi"
 fave_food2 = "pizza"
 print("I love to eat {} and {}" .format(fave_food1, fave_food2)) # with .format()
 print(f"I love to eat {fave_food1} and {fave_food2}") # with an f string
+
+
+first_name = "Mike"
+last_name = "Magruder"
+
+print(f"Hello my name is {first_name} {last_name}")
+
+print("Hello my name is {} {}" .format(first_name, last_name))
+
+name = "Mike"
+print("My name is",name)
+print("My name is " + name)
+num = 42
+print("The number is",num)
+print("The number is " + str(num))
+
+##################
+
+x = [1,2,3,4,5]
+y = [10, 4, 6, 3, 101]
+
+for i in x:
+    print(enumerate(x))
+    
+def print_nums(x):
+    print(x * 2)
+        
+map(print_nums(x), x)
+
+print(min(x))
+print(max(x))
+print(sorted(y))
+
+print(sum(x))
